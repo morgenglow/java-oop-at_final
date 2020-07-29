651f653d8d8223fc8b357f1c43c2999516b85f4e
Antonpavlov <anton.it.pavlov@gmail.com>
2020-07-29 22:51:21 +0300

    код из урока 7

--- src/main/java/ru/geekbrains/example/incheckexception/ArithmeticExceptionExample.java
+++
@@ -1,8 +0,0 @@
-package ru.geekbrains.example.incheckexception;
-
-public class ArithmeticExceptionExample {
-    public static void main(String[] args) {
-        System.out.println(5 / 0);
-    }
-}
-
