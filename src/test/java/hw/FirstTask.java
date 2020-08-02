b3389e0dc697180a14c9bd2a63bd88b12992dbdf
Antonpavlov <anton.it.pavlov@gmail.com>
2020-08-02 19:10:35 +0300

    изменил тип коллекции на интерфейс

--- src/test/java/hw/FirstTask.java
+++ src/test/java/hw/FirstTask.java
@@ -16,7 +16,7 @@ public class FirstTask {
         String text = "Ведьмаку заплатите чеканной монетой чеканной монетой Ведьмаку заплатите зачтется все это вам";
         String[] array = text.split(" ");
         List<String> list = Arrays.asList(array);
-        HashSet<String> set = new HashSet<>(list);
+        Set<String> set = new HashSet<>(list);
 
         for (String word : set) {
             System.out.println("Слово '" + word + "' встречается " + Collections.frequency(list, word) + " раз(а).");
